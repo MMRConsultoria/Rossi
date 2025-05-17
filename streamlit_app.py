114407452c49259e1317747aa50f946595749513
MMR Consultoria <maricelisrossi@gmail.com>
2025-05-17 10:21:58 -0300

    Create streamlit_app.py

---
+++ streamlit_app.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+st.set_page_config(page_title="Dashboard Cliente XYZ", layout="wide")
+
+st.title("📊 Relatório Gerencial - Cliente XYZ")
+
+st.write("Bem-vindo ao painel de relatórios interativos.")
